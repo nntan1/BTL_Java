66e20dfcdeadc30f0321b114328feb827bca8111
nntan1 <166012989+nntan1@users.noreply.github.com>
2024-05-26 21:45:45 +0700

    Update DangNhapEntity.java

--- src/Entity/DangNhapEntity.java
+++ src/Entity/DangNhapEntity.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Entity;
 
 
@@ -32,38 +31,4 @@ public class DangNhapEntity
         this.IsAdmin = IsAdmin;
     }
 }
-=======
-package Entity;
 
-
-public class DangNhapEntity 
-{
-    private String Username;
-    private String Pwd;
-    private int IsAdmin;
-
-    public String getUsername() {
-        return Username;
-    }
-
-    public void setUsername(String Username) {
-        this.Username = Username;
-    }
-
-    public String getPwd() {
-        return Pwd;
-    }
-
-    public void setPwd(String Pwd) {
-        this.Pwd = Pwd;
-    }
-
-    public int getIsAdmin() {
-        return IsAdmin;
-    }
-
-    public void setIsAdmin(int IsAdmin) {
-        this.IsAdmin = IsAdmin;
-    }
-}
->>>>>>> f166b83cb113f9fee87da2f56c50c520d36c8522
