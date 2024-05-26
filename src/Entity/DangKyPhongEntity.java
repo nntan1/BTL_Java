e9751c9c03795001d9cfbea4b49eeee0e4024c41
nntan1 <166012989+nntan1@users.noreply.github.com>
2024-05-26 21:45:21 +0700

    Update DangKyPhongEntity.java

--- src/Entity/DangKyPhongEntity.java
+++ src/Entity/DangKyPhongEntity.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Entity;
 
 import java.sql.Date;
@@ -43,49 +42,3 @@ public class DangKyPhongEntity
         ngayvaophong = _ngayvaophong;
     }
 }
-=======
-package Entity;
-
-import java.sql.Date;
-
-
-
-public class DangKyPhongEntity 
-{
-    private String maphong;
-    private String makhach;
-    private String ngayvaophong;
-
-    public String getMaPhong()
-    {
-        return maphong;
-    }
-    
-    public void setMaPhong(String _maphong)
-    {
-        maphong = _maphong;
-    }
-    
-    //Mã khách
-    public String getMaKhach()
-    {
-        return makhach;
-    }
-    
-    public void setMaKhach(String _makhach)
-    {
-        makhach = _makhach;
-    }
-       
-    //Mã phòng
-    public String getNgayVaoPhong()
-    {
-        return ngayvaophong;
-    }
-    
-    public void setNgayVaoPhong(String _ngayvaophong)
-    {
-        ngayvaophong = _ngayvaophong;
-    }
-}
->>>>>>> f166b83cb113f9fee87da2f56c50c520d36c8522
